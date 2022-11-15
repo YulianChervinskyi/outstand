c29fd6e91e72596a798970cfa4260629cea6c4f5
pavlo <cpavlo@gmail.com>
2022-11-15 08:59:21 +0200

    fix layout errors

--- src/calc/KeyLayout.tsx
+++ src/calc/KeyLayout.tsx
@@ -15,11 +15,13 @@ export function KeyLayout(props: IKeyLayoutProps) {
                 <div style={{display: "flex", flexDirection: "column", width: "75%", justifyContent: "space-evenly"}}>
                     <div style={{display: "flex", flexDirection: "row", height: "100%"}}>
                         {[Digit.D7, Digit.D8, Digit.D9].map((d) =>
-                            <button style={{width: "33.33%"}} onClick={() => props.onPressDigit(d)}>{d}</button>)}
+                            <button key={d} style={{width: "33.33%"}}
+                                    onClick={() => props.onPressDigit(d)}>{d}</button>)}
                     </div>
                     <div style={{display: "flex", flexDirection: "row", height: "100%"}}>
                         {[Digit.D4, Digit.D5, Digit.D6].map((d) =>
-                            <button style={{width: "33.33%"}} onClick={() => props.onPressDigit(d)}>{d}</button>)}
+                            <button key={d} style={{width: "33.33%"}}
+                                    onClick={() => props.onPressDigit(d)}>{d}</button>)}
                     </div>
                 </div>
                 <button style={{width: "25%"}} onClick={() => props.onPressOperator(Operator.Plus)}>+</button>
@@ -29,7 +31,8 @@ export function KeyLayout(props: IKeyLayoutProps) {
                 <div style={{display: "flex", flexDirection: "column", width: "75%", justifyContent: "space-evenly"}}>
                     <div style={{display: "flex", flexDirection: "row", height: "100%"}}>
                         {[Digit.D1, Digit.D2, Digit.D3].map((d) =>
-                            <button style={{width: "33.33%"}} onClick={() => props.onPressDigit(d)}>{d}</button>)}
+                            <button key={d} style={{width: "33.33%"}}
+                                    onClick={() => props.onPressDigit(d)}>{d}</button>)}
                     </div>
                     <div style={{display: "flex", flexDirection: "row", height: "100%"}}>
                         <button style={{width: "66.66%"}} onClick={() => props.onPressDigit(Digit.D0)}>0</button>
