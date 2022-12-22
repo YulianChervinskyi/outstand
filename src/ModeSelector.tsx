819cffe47f6559bff52eebf4d077dab664d992aa
Yulian <yulian2002@gmail.com>
2022-12-22 20:50:29 +0200

    change mode-selector

--- src/ModeSelector.tsx
+++ src/ModeSelector.tsx
@@ -1,5 +1,5 @@
 import {BoxType} from "./Box";
-import React from "react";
+import React, {useState} from "react";
 import './ModeSelector.css';
 
 export interface IModeSelectorProps {
@@ -8,11 +8,23 @@ export interface IModeSelectorProps {
 }
 
 export function ModeSelector(props: IModeSelectorProps) {
+    const [btnDisplay, setBtnDisplay] = useState<boolean>(false);
+
+    // const handleClick = (value: BoxType) => {
+    //     props.onSelectMode(value);
+    //     setStyle({
+    //         filter: props.selected === value ? "none" : "grayscale(100%)",
+    //     });
+    // }
+
     return (
-        <div className="mode-selector">
+        <div className="mode-selector"
+             onMouseEnter={() => setBtnDisplay(true)}
+             onMouseLeave={() => setBtnDisplay(false)}>
             {Object.values(BoxType).map((value) =>
                 <button
-                    style={{borderColor: props.selected === value ? 'lightsalmon' : 'darkgray'}}
+                    style={{filter: props.selected === value ? "none" : "grayscale(100%)",
+                        display: btnDisplay ? "inline-block" : "none"}}
                     onClick={() => props.onSelectMode(value)}>
                     {value}
                 </button>)}
