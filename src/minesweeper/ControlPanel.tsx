ad124027db9c1aa6b2d4af83d397d735a80329df
pavlo <cpavlo@gmail.com>
2023-01-02 23:23:20 +0200

    cleanup

--- src/minesweeper/ControlPanel.tsx
+++ src/minesweeper/ControlPanel.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useState} from "react";
 import './ControlPanel.css';
 
 enum DifficultyType {
@@ -8,41 +8,20 @@ enum DifficultyType {
 }
 
 export function ControlPanel() {
-    const [currentDifficulty, setCurrentDifficulty] = useState(DifficultyType.Easy);
-    const [isMenuVisible, setIsMenuVisible] = useState(false);
+    const [difficulty, setDifficulty] = useState(DifficultyType.Easy);
 
-    const ref = useRef<HTMLDivElement>(null);
-
-    const handleClickOutside = (event: MouseEvent) => {
-        if (ref.current && !ref.current.contains(event.target as HTMLElement)) {
-            console.log("click outside after check");
-            setIsMenuVisible(false);
-        }
-    };
-
-    const handleSelectDifficulty = (value: DifficultyType) => {
-        setCurrentDifficulty(value);
-        setIsMenuVisible(!isMenuVisible);
+    const handleChangeDifficulty = (value: DifficultyType) => {
+        setDifficulty(value);
     }
-
-    useEffect(() => {
-        document.addEventListener("click", handleClickOutside, true);
-        return () => {
-            document.removeEventListener("click", handleClickOutside, true);
-        };
-    });
 
     return (
         <div className="controlPanel">
-            <div className="menu" ref={ref}>
-                <button onClick={() => setIsMenuVisible(!isMenuVisible)}>{currentDifficulty}</button>
-                <div style={{display: isMenuVisible ? "block" : "none"}} >
-                    {Object.values(DifficultyType).map((value) =>
-                        <button onClick={() => handleSelectDifficulty(value)}>
-                            {value}
-                        </button>)}
-                </div>
-            </div>
+            <select value={difficulty}
+                    onChange={(e) => handleChangeDifficulty(e.target.value as DifficultyType)}>
+                {Object.values(DifficultyType).map((value) =>
+                    <option>{value}</option>
+                )}
+            </select>
         </div>
     );
 }
