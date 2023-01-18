702494b2369340c484d9311f4ba8b7070af924fc
Yulian <yulian2002@gmail.com>
2023-01-18 12:58:52 +0200

    config

--- src/minesweeper/config.ts
+++ src/minesweeper/config.ts
@@ -16,7 +16,7 @@ export interface ICell {
 }
 
 export const gameProps = {
-    [EDifficultyType.Easy]: {height: 8, width: 8, mines: 2},
+    [EDifficultyType.Easy]: {height: 8, width: 8, mines: 10},
     [EDifficultyType.Medium]: {height: 16, width: 16, mines: 40},
     [EDifficultyType.Hard]: {height: 16, width: 30, mines: 99},
 }
