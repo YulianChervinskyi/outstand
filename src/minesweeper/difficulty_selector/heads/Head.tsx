dcc377ce5d6e5b76c9a354162e82162663e72d8c
pavlo <cpavlo@gmail.com>
2023-02-01 01:08:26 +0200

    open mouth

--- src/minesweeper/difficulty_selector/heads/Head.tsx
+++ src/minesweeper/difficulty_selector/heads/Head.tsx
@@ -1,7 +1,6 @@
 import "./Head.css";
 import {Eye} from "./Eye";
 import {EDifficultyType} from "../../config";
-import {useState} from "react";
 
 export interface IFaceProps {
     background: string,
@@ -10,25 +9,22 @@ export interface IFaceProps {
 }
 
 export function Head(props: IFaceProps) {
-    const [showTeeth, setShowTeeth] = useState(false);
 
     return (
         <div className="head rounded"
              style={{backgroundColor: props.background}}
              onClick={() => props.giveDifficulty(props.difficulty)}
-             onMouseEnter={() => setShowTeeth(true)}
-             onMouseLeave={() => setShowTeeth(false)}
         >
             <div className="v-half-head center-elements">
                 <Eye/>
                 <Eye/>
             </div>
             <div className="v-half-head center-elements">
-                <div className="mouth" style={{backgroundColor: showTeeth ? "#7e1212" : "#a80808"}}>
-                    {showTeeth && <div className="teeth center-elements">
+                <div className="mouth">
+                    <div className="teeth center-elements">
                         <div id="left-tooth" className="tooth"></div>
                         <div id="right-tooth" className="tooth"></div>
-                    </div>}
+                    </div>
                 </div>
             </div>
         </div>
