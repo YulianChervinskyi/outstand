400070cac180cdea0548b26417ffa2a6f0c24aa2
Yulian <yulian2002@gmail.com>
2023-02-22 00:19:05 +0200

    bomb spawn

--- src/bomberman/models/GameModel.ts
+++ src/bomberman/models/GameModel.ts
@@ -27,7 +27,7 @@ export class GameModel {
                 else if ((x + y <= 1) || (x + y) >= (this.height + this.width - 3))
                     this.field[y][x] = ECellType.Empty;
                 else
-                    this.field[y][x] = Math.random() < 0.1 ? ECellType.Wall : ECellType.Empty;
+                    this.field[y][x] = Math.random() < 0.05 ? ECellType.Wall : ECellType.Empty;
             }
         }
     }
@@ -41,9 +41,22 @@ export class GameModel {
 
     checkPlayerOffset(player: PlayerModel, offset: { x: number, y: number }) {
         let validOffset = {x: 0, y: 0};
+
         const pos = player.pos;
+
         const col = Math.round(pos.x);
         const row = Math.round(pos.y);
+
+        const distanceFromPlayerToCell_X = Math.abs(Math.round(pos.x) - pos.x);
+        const distanceFromPlayerToCell_Y = Math.abs(Math.round(pos.y) - pos.y);
+
+        const checkPlayerPos = () => {
+            if (pos.y % 1 === 0 && pos.x % 1 !== 0)
+                validOffset.x = offset.x;
+
+            if (pos.x % 1 === 0 && pos.y % 1 !== 0)
+                validOffset.y = offset.y;
+        }
 
         if (pos.x + offset.x < 0)
             offset.x = -pos.x;
@@ -54,19 +67,21 @@ export class GameModel {
         if (pos.y + offset.y > this.height - 1)
             offset.y = this.height - 1 - pos.y;
 
-        const distanceFromPlayerToCell_X = Math.abs(Math.round(pos.x) - pos.x);
-        const distanceFromPlayerToCell_Y = Math.abs(Math.round(pos.y) - pos.y);
-
         if (distanceFromPlayerToCell_X && distanceFromPlayerToCell_X < Math.abs(offset.x))
             offset.x = distanceFromPlayerToCell_X * Math.sign(offset.x);
         if (distanceFromPlayerToCell_Y && Math.abs(distanceFromPlayerToCell_Y) < Math.abs(offset.y))
             offset.y = distanceFromPlayerToCell_Y * Math.sign(offset.y);
 
-        const newCol = Math.round(pos.x + offset.x + Math.sign(offset.x) / 2);
-        const newRow = Math.round(pos.y + offset.y + Math.sign(offset.y) / 2);
+        const newCol = pos.x + offset.x < this.width - 1
+            ? Math.round(pos.x + offset.x + Math.sign(offset.x) / 2)
+            : this.width - 1;
 
-        if (newCol > this.width - 1 || newCol < 0 || newRow > this.height - 1 || newRow < 0)
-            return {x: offset.x, y: offset.y};
+        const newRow = pos.y + offset.y < this.height - 1
+            ? Math.round(pos.y + offset.y + Math.sign(offset.y) / 2)
+            : this.height - 1;
+
+        if (this.field[row][col] !== ECellType.Empty)
+            checkPlayerPos();
 
         if (pos.y % 1 === 0 && this.field[row][newCol] === ECellType.Empty)
             validOffset.x = offset.x;
@@ -80,6 +95,9 @@ export class GameModel {
 
         if (validOffset.x && validOffset.y)
             validOffset = player.prevAxis === "x" ? {x: 0, y: validOffset.y} : {x: validOffset.x, y: 0};
+
+        console.log(`pos: ${pos.x}, ${pos.y}`);
+        console.log(`posRounded: ${col}, ${row}`);
 
         return validOffset;
     }
