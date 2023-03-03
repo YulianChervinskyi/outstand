26063a592b0efbf3774326faba0694fbbcd9e3e9
pavlo <cpavlo@gmail.com>
2023-03-03 11:23:49 +0200

    event listeners

--- src/bomberman/models/BombModel.ts
+++ src/bomberman/models/BombModel.ts
@@ -4,7 +4,30 @@ export class BombModel {
     readonly spawnTime = performance.now() / 1000;
     readonly spawnPos: IPoint = {x: 0, y: 0};
 
+    private listeners: { [key: string]: ((event: BombModel) => void)[] } = {};
+
     constructor(pos: { x: number, y: number }) {
         this.spawnPos = {x: pos.x, y: pos.y};
     }
+
+    addEventListener(event: "onExplosion", callback: (event: BombModel) => void) {
+        this.listeners[event] = this.listeners[event] || [];
+        if (this.listeners[event].includes(callback))
+            return;
+
+        this.listeners[event].push(callback);
+    }
+
+    removeEventListener(event: "onExplosion", callback: (event: BombModel) => void) {
+        if (!this.listeners[event])
+            return;
+
+        this.listeners[event] = this.listeners[event].filter((listener) => listener !== callback);
+    }
+
+    private explosion() {
+        // TODO add explosion logic
+        this.listeners["onExplosion"]?.forEach((listener) => listener(this));
+    }
+
 }
