49c1906958ba5aae0175c8cceec79a6098359a59
Yulian <yulian2002@gmail.com>
2023-03-23 23:19:24 +0200

    explosion spawn() refactor

--- src/bomberman/models/ExplosionModel.ts
+++ src/bomberman/models/ExplosionModel.ts
@@ -51,32 +51,25 @@ export class ExplosionModel implements ISceneObject {
     }
 
     private spawn() {
-        // TODO: try to refactor this
         if (this.power <= 0)
             return;
 
-        const getNextExplosionPower = (pos: IPoint) => {
-            return [ECellType.Empty, ECellType.Bonus, ECellType.Explosion].includes(this.field[pos.y][pos.x]) ? this.power - 1 : 0;
-        }
+        for (let y = 0; y < 2; y++) {
+            for (let x = -1; x < 2; x += 2) {
+                const direction = this.direction ? this.direction : {x: x * (1 - y), y: y * x};
+                const pos = {x: this.pos.x + direction.x, y: this.pos.y + direction.y};
 
-        if (!this.direction) {
-            for (let y = 0; y < 2; y++) {
-                for (let x = -1; x < 2; x += 2) {
-                    const direction = {x: x * (1 - y), y: y * x};
-                    const pos = {x: this.pos.x + direction.x, y: this.pos.y + direction.y};
+                if (this.field[pos.y]?.[pos.x] === undefined || this.field[pos.y]?.[pos.x] === ECellType.AzovSteel)
+                    continue;
 
-                    if (this.field[pos.y]?.[pos.x] !== undefined && this.field[pos.y]?.[pos.x] !== ECellType.AzovSteel)
-                        this.createExplosion(pos, direction, getNextExplosionPower(pos));
-                }
+                const power = [ECellType.Empty, ECellType.Bonus, ECellType.Explosion].includes(this.field[pos.y][pos.x]) ? this.power - 1 : 0;
+                this.createExplosion(pos, direction, power);
+
+                if (direction === this.direction)
+                    break;
             }
-        } else {
-            const pos = {x: this.pos.x + this.direction.x, y: this.pos.y + this.direction.y};
-
-            if (this.field[pos.y]?.[pos.x] === undefined || this.field[pos.y]?.[pos.x] === ECellType.AzovSteel)
-                return;
-
-            this.createExplosion(pos, this.direction, getNextExplosionPower(pos));
         }
+
         this.power = 0;
     }
 }
