193228836e946f5c818c878c303a7ce6d171b07b
pavlo <cpavlo@gmail.com>
2023-04-10 00:27:32 +0300

    do not shake info panel

--- src/bomberman/info_panel/InfoPanel.tsx
+++ src/bomberman/info_panel/InfoPanel.tsx
@@ -2,14 +2,23 @@ import "./InfoPanel.scss";
 import {IPlayerStats} from "../types";
 
 export function InfoPanel(props: { stats: IPlayerStats }) {
+
+    const parseObject = (obj: object) => {
+        return Object.entries(obj).map(([key, value]) =>
+            <div key={key}>
+                {`${key}`}
+                <div>{
+                    value instanceof Object
+                        ? parseObject(value)
+                        : typeof (value) === "number" ? value.toFixed(4) : value
+                }</div>
+            </div>)
+    }
+
     return (
         <div className="info-panel">
             <div className="player-stats">
-                {Object.entries(props.stats).map(([key, value]) =>
-                    <div key={key}>
-                        {`${key}`}
-                        <div>{`${(value instanceof Object) ? JSON.stringify(value) : value}`}</div>
-                    </div>)}
+                {parseObject(props.stats)}
             </div>
         </div>
     );
