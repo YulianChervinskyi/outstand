356fbf944e1deec73c9b37f71f93006ea6acbcb8
pavlo <cpavlo@gmail.com>
2023-04-12 21:26:36 +0300

    mark changed values for 1 second

--- src/bomberman/info_panel/InfoPanel.tsx
+++ src/bomberman/info_panel/InfoPanel.tsx
@@ -3,16 +3,24 @@ import {IPlayerStats} from "../types";
 import {useEffect, useRef} from "react";
 
 function StateProp(props: { name: string, value: any }) {
-    const value = useRef(null);
+    const value = useRef<any>(null);
+    const activeUntil = useRef<number>(0);
 
     useEffect(() => {
-        if (value.current !== props.value)
+        if (value.current !== props.value) {
             value.current = props.value;
+            activeUntil.current = Date.now() + 1000;
+        }
     });
+
+    const isActive = activeUntil.current > Date.now();
+    const style = isActive
+        ? {fontWeight: "bold", color: "lightgoldenrodyellow"}
+        : {fontWeight: "inherit", color: "inherit"};
 
     return <div className="state-prop">
         <div>{props.name}</div>
-        <div style={value.current === props.value ? {fontWeight: "inherit", color: "inherit"} : {fontWeight:  "bold", color: "lightgoldenrodyellow"}}>
+        <div style={style}>
             {typeof props.value === "number" ? props.value.toFixed(1) : JSON.stringify(props.value)}
         </div>
     </div>
