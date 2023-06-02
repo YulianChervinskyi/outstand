f134596d6d66e137b469cf63062d59ae6b5f8a9d
Yulian <yulian2002@gmail.com>
2023-06-02 21:43:31 +0300

    bonus visual

--- src/bomberman/assets/index.ts
+++ src/bomberman/assets/index.ts
@@ -3,10 +3,26 @@ import azov_steel from "./img/azov_steel.png";
 import bomb from "./img/bomb.png";
 import player from "./img/player.png";
 
+import lottery from "./img/bonuses/lottery.png";
+import push from "./img/bonuses/push.png";
+import spam from "./img/bonuses/spam.png";
+import speed from "./img/bonuses/speed.png";
+import power from "./img/bonuses/power.png";
+import supply from "./img/bonuses/supply.png";
+
 export const images = {
     wall,
     azov_steel,
     bomb,
 };
+
+export const bonus_img = {
+    lottery,
+    push,
+    spam,
+    speed,
+    power,
+    supply,
+}
 
 export {player};
--- src/bomberman/bonus/Bonus.tsx
+++ src/bomberman/bonus/Bonus.tsx
@@ -1,9 +1,33 @@
 import {BonusModel} from "../models/BonusModel";
 import {EBonusType} from "../types";
+import {bonus_img} from "../assets";
+import React from "react";
 import "./Bonus.scss";
 
 export function Bonus(props: { bonus: BonusModel }) {
-    return <div className="bonus">
-        <p>{EBonusType[props.bonus._type]}</p>
-    </div>;
+    const value = Math.round(performance.now() / 10);
+
+    const getSrc = () => {
+        switch (props.bonus._type) {
+            case EBonusType.Lottery:
+                return bonus_img.lottery;
+            case EBonusType.Push:
+                return bonus_img.push;
+            case EBonusType.Supply:
+                return bonus_img.supply;
+            case EBonusType.Speed:
+                return bonus_img.speed;
+            case EBonusType.Spam:
+                return bonus_img.spam;
+            case EBonusType.Power:
+                return bonus_img.power;
+        }
+    }
+
+    return <img
+        style={{background: `repeating-conic-gradient(from ${value}deg at 50%, #FF0000 0deg 10deg, #FFA500 10deg 20deg, #FFFF00 20deg 30deg, #7FFF00 30deg 40deg, #00FFFF 40deg 50deg, #0000FF 50deg 60deg, #9932CC 60deg 70deg, #FF1493 70deg 80deg)`}}
+        className="bonus"
+        src={getSrc()}
+        alt="wall"
+    />
 }
